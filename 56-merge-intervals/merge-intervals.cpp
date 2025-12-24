b619965900e86105d9f887549321d849a93e2a12
its-biplab <biplabsaha2005@gmail.com>
2025-12-24 19:22:43 +0530

    Time: 8 ms (26.85%) | Memory: 23.9 MB (33.94%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>ans;
+        
+        if(intervals.size() == 0){
+            return ans;
+        }
+
+        sort(intervals.begin(),intervals.end());
+            ans.push_back(intervals[0]);
+
+            for(int i = 1; i < intervals.size(); i++){
+
+                if(intervals[i][0] <= ans.back()[1]){
+                    ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+                }else{
+                    ans.push_back(intervals[i]);
+                }
+            }
+
+
+        // int n = intervals.size();
+        // while(i<n && intervals[i][1] < intervals[i+1][0]){
+        //     ans.push_back(intervals[i]);
+        // }
+        // while(i < n){
+        //     intervals[i][0] = min(intervals[i][0], intervals[i+1][0]);
+        //     intervals[i][1] = max(intervals[i][1], intervals[i+1][1]);
+        //     ans.push_back(intervals[i]);
+        // }
+
+
+
+        // {
+        //     if(intervals[i][1] < intervals[i+1][0]){
+        //         ans.push_back(intervals[i]);
+        //     }else{
+        //         intervals[i][0] = min(intervals[i][0], intervals[i+1][0]);
+        //          intervals[i][1] = max(intervals[i][1], intervals[i+1][1]);
+        //           ans.push_back(intervals[i]);
+        //     }
+        // }
+        return ans;
+    }
+};
