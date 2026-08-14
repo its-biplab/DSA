bb06eca3ac4b909d39b0943a46ce705e610bda97
its-biplab <biplabsaha2005@gmail.com>
2026-08-14 10:14:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (49.95%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,26 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
+        /* sort(s.begin(),s.end());
         sort(t.begin(),t.end());
         if(s == t) return true;
         return false;
+        */
+
+        unordered_map<int,int>st;
+        if(s.length() != t.length()) return false;
+
+        for(int x : s){
+            st[x]++;
+        }
+
+        for(int x : t){
+            st[x]--;
+        }
+
+        for(int x : s){
+            if(st[x] > 0) return false;
+        }
+        return true;
     }
 };
