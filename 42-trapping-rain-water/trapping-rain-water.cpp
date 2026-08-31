e1209851995d4b4aad4e18d8d1a7f507ad29a7d1
its-biplab <biplabsaha2005@gmail.com>
2026-09-01 01:13:09 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (56.11%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n= height.size();
-        vector<int> lmax(n,0);
-        vector<int> rmax(n,0);
-        lmax[0] = height[0];
-        rmax[n-1] = height[n-1];
-        for(int i=1; i<n; i++){
-            lmax[i] = max(lmax[i-1],height[i]);
+        // int n= height.size();
+        // vector<int> lmax(n,0);
+        // vector<int> rmax(n,0);
+        // lmax[0] = height[0];
+        // rmax[n-1] = height[n-1];
+        // for(int i=1; i<n; i++){
+        //     lmax[i] = max(lmax[i-1],height[i]);
+        // }
+        // for(int i=n-2; i>=0; i--){
+        //     rmax[i] = max(rmax[i+1],height[i]);
+        // }
+        // int ans = 0;
+        // for(int i=0; i<n; i++){
+        //     ans += (min(lmax[i],rmax[i]) - height[i]);
+        // }
+        // return ans;
+
+
+        int left = 0;
+        int right = height.size() - 1;
+        int leftMax = height[left];
+        int rightMax = height[right];
+        int water = 0;
+        while(left < right){
+            if(leftMax < rightMax){
+                left++;
+                leftMax = max(leftMax,height[left]);
+                water += leftMax - height[left];
+            }else{
+                right--;
+                rightMax = max(rightMax,height[right]);
+                water += rightMax - height[right];
+            }
         }
-        for(int i=n-2; i>=0; i--){
-            rmax[i] = max(rmax[i+1],height[i]);
-        }
-        int ans = 0;
-        for(int i=0; i<n; i++){
-            ans += (min(lmax[i],rmax[i]) - height[i]);
-        }
-        return ans;
+        return water;
     }
 };
