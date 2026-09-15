3f0de70512ac3e0de467f957256425cff8ca0964
its-biplab <biplabsaha2005@gmail.com>
2026-09-16 01:59:07 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (5.29%) - LeetSync

--- 289-game-of-life/game-of-life.cpp
+++ 289-game-of-life/game-of-life.cpp
@@ -4,7 +4,7 @@ vector<vector<int>>dir = {{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1}}
     void gameOfLife(vector<vector<int>>& board) {
      int m = board.size();
      int n = board[0].size();
-     vector<vector<int>>res(m, vector<int>(n));
+    /* vector<vector<int>>res(m, vector<int>(n)); */
      for(int i = 0; i < m; i++){
         for(int j = 0; j < n; j++){
             int count = 0;
@@ -12,19 +12,31 @@ vector<vector<int>>dir = {{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1}}
                 int p = i + d[0];
                 int q = j + d[1];
             
-            if(p >= 0 && p < m && q >= 0 && q < n && board[p][q] == 1){
+           /* if(p >= 0 && p < m && q >= 0 && q < n && board[p][q] == 1){
+                count++;
+            }
+            */
+             if(p >= 0 && p < m && q >= 0 && q < n && (board[p][q] == 1 || board[p][q] == 2)){
                 count++;
             }
         }
         if(board[i][j] == 1){
+            /*
             if(count == 2 || count == 3){
               res[i][j] = 1;  
             }
             else res[i][j] = 0;
+            */
+            if(count < 2 || count > 3){
+              board[i][j] = 2;  // 2 meaen ALIVE->DEAD
+            }
+            else{
+                board[i][j] = 1;
+            }
         }
         else{
             if(count == 3){
-                res[i][j] = 1;
+                board[i][j] = 3; // 3 mean DEAD->ALIVE
             }
         }
 
@@ -32,7 +44,8 @@ vector<vector<int>>dir = {{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1}}
      }
      for(int i = 0; i < m; i++){
      for(int j = 0; j < n; j++){
-        board[i][j] = res[i][j];
+        // board[i][j] = res[i][j];
+        board[i][j] %= 2;
      }}
 
     }
