951cdf734a2e386cb4360c1ee071081d93aec690
its-biplab <biplabsaha2005@gmail.com>
2026-09-16 01:46:48 +0530

    Time: 3 ms (4.45%) | Memory: 10 MB (5.29%) - LeetSync

---
+++ 289-game-of-life/game-of-life.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+vector<vector<int>>dir = {{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1}};
+    void gameOfLife(vector<vector<int>>& board) {
+     int m = board.size();
+     int n = board[0].size();
+     vector<vector<int>>res(m, vector<int>(n));
+     for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            int count = 0;
+            for(auto d : dir){
+                int p = i + d[0];
+                int q = j + d[1];
+            
+            if(p >= 0 && p < m && q >= 0 && q < n && board[p][q] == 1){
+                count++;
+            }
+        }
+        if(board[i][j] == 1){
+            if(count == 2 || count == 3){
+              res[i][j] = 1;  
+            }
+            else res[i][j] = 0;
+        }
+        else{
+            if(count == 3){
+                res[i][j] = 1;
+            }
+        }
+
+        }
+     }
+     for(int i = 0; i < m; i++){
+     for(int j = 0; j < n; j++){
+        board[i][j] = res[i][j];
+     }}
+
+    }
+};
