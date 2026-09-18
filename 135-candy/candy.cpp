ecb86612b47eb39b23982de4c55aae3c72959271
its-biplab <biplabsaha2005@gmail.com>
2026-09-18 21:16:08 +0530

    Time: 0 ms (100.00%) | Memory: 68.1 MB (56.40%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
+        /**
         int n = ratings.size();
         vector<int>candy(n,1);
         for(int i=1; i<n; i++){
@@ -14,11 +15,40 @@ public:
                 candy[i] = max(candy[i],candy[i+1]+1);
             }
         }
-
+                                                        //T.C = O(3N), S.C = O(N)
         int sum = 0;
         for(int c : candy){
             sum += c;
         }
         return sum;
+        */
+
+    int n = ratings.size();
+    int sum = 1;
+    int i = 1;
+    while(i < n){
+        if(ratings[i] == ratings[i-1]){
+            sum += 1;
+            i++;
+            continue;
+        }
+        int peak = 1;
+        while(i < n && ratings[i] > ratings[i-1]){
+            peak += 1;
+            sum += peak;
+            i++;
+        }
+        int down = 1;
+        while(i < n && ratings[i] < ratings[i-1]){
+            sum += down;
+            down += 1;
+            i++;
+        }
+        if(down > peak){
+            sum += down - peak;
+        }
+    }
+        return sum;
+
     }
 };
