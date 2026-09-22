ff38d5cae66ed6679905e002ee25ad800f68fb32
its-biplab <biplabsaha2005@gmail.com>
2026-09-22 21:21:20 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (91.90%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& nums, int k) {
-        int n = nums.size();
+    /*    int n = nums.size();
         for(int i = 0; i < n; i++){
             if(nums[i] <= k) k++;
             else{
@@ -9,5 +9,16 @@ public:
             }
         }
         return k;
+    */
+    int n = nums.size();
+    int low = 0;
+    int high = n-1;
+    while(low <= high){
+        int mid = low + (high-low)/2;
+        int missing = nums[mid] - (mid+1);
+        if(missing < k) low = mid + 1;
+        else high = mid -1;
+    } 
+    return low + k;
     }
 };
